489346020b1c2cb0ffd9f3277ed0bd14722c0f2c
Chroma9 <64189171+chroma9@users.noreply.github.com>
2022-08-11 16:13:58 -0400

    Update races.js

--- assets/js/races.js
+++ assets/js/races.js
@@ -2,12 +2,126 @@
 
 let currentRecords = {};
 
-currentRecords['second'] = [];
-currentRecords['stadium'] = [];
-currentRecords['highway'] = [];
-currentRecords['chuo'] = [];
-currentRecords['dogenzaka'] = [];
-currentRecords['kibogaoka'] = [];
+currentRecords[`second`] = [
+    {
+        char: { id: `jacket`, name: `Jacket` },
+        name: `Veloci`,
+        time: `03:27:943`,
+        date: `June 3, 2022`
+    },
+    {
+        char: { id: `jacket`, name: `Jacket` },
+        name: `Latelaturi`,
+        time: `03:28:589`,
+        date: `May 24, 2022`
+    },
+    {
+        char: { id: `jazz`, name: `Jazz` },
+        name: `Sharkbite`,
+        time: `03:31:156`,
+        date: `April 21, 2021`
+    }
+];
+currentRecords[`stadium`] = [
+    {
+        char: { id: `immortal`, name: `Immortal` },
+        name: `Latelaturi`,
+        time: `02:18:936`,
+        date: `June 6, 2022`
+    },
+    {
+        char: { id: `jacket`, name: `Jacket` },
+        name: `Veloci`,
+        time: `02:19:788`,
+        date: `May 20, 2022`
+    },
+    {
+        char: { id: `immortal`, name: `Immortal` },
+        name: `MaZaOsMe`,
+        time: `02:20:867`,
+        date: `July 28, 2022`
+    },
+];
+currentRecords[`highway`] = [
+    {
+        char: { id: `jacket`, name: `Jacket` },
+        name: `Veloci`,
+        time: `02:13:771`,
+        date: `May 20, 2022`
+    },
+    {
+        char: { id: `jacket`, name: `Jacket` },
+        name: `Latelaturi`,
+        time: `02:14:941`,
+        date: `May 24, 2022`
+    },
+    {
+        char: { id: `unknown`, name: `Unknown` },
+        name: `Sharkbite`,
+        time: `02:16:734`,
+        date: `March 28, 2021`
+    },
+];
+currentRecords[`chuo`] = [
+    {
+        char: { id: `goldenrhino`, name: `Golden Rhino` },
+        name: `Veloci`,
+        time: `03:52:531`,
+        date: `May 20, 2022`
+    },
+    {
+        char: { id: `jacket`, name: `Jacket` },
+        name: `Latelaturi`,
+        time: `03:53:975`,
+        date: `May 21, 2022`
+    },
+    {
+        char: { id: `jacket`, name: `Jacket` },
+        name: `'p`,
+        time: `03:54:586`,
+        date: `May 20, 2022`
+    },
+];
+currentRecords[`dogenzaka`] = [
+    {
+        char: { id: `coin`, name: `Coin` },
+        name: `Veloci`,
+        time: `03:26:839`,
+        date: `July 7, 2022`
+    },
+    {
+        char: { id: `jacket`, name: `Jacket` },
+        name: `Latelaturi`,
+        time: `03:27:777`,
+        date: `July 5, 2022`
+    },
+    {
+        char: { id: `jacket`, name: `Jacket` },
+        name: `TacoLover`,
+        time: `03:28:985`,
+        date: `October 4, 2021`
+    },
+];
+currentRecords[`kibogaoka`] = [
+    {
+        char: { id: `jacket`, name: `Jacket` },
+        name: `Latelaturi`,
+        time: `04:07:729`,
+        date: `July 5, 2021`
+    },
+    {
+        char: { id: `goldenrhino`, name: `Golden Rhino` },
+        name: `Veloci`,
+        time: `04:12:695`,
+        date: `July 20, 2022`
+    },
+    {
+        char: { id: `unknown`, name: `Unknown` },
+        name: `Buggy`,
+        time: `04:16:626`,
+        date: `June 7, 2022`
+    },
+];
 
 // Compiling Records to Page
 (function() {
